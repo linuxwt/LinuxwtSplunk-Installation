51978d16d3b34934589bb4fc5f51f386b7632463
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-05-12 22:35:27 +0800

    Create remote_auto.sh

---
+++ remote_auto.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#######remote auto install Splunk Enterprise7.2.6###########
+######ip.txt storage remote server ip address and root password########
+#####default Splunk dir "/data"  user "wangshan" password "wangshan123"#######
+#####splunk_install.sh is local script#################
+
+remotesh () {
+/usr/bin/expect << EOF
+set timeout 200
+spawn scp splunk_install.sh ${ip}:/root
+expect "password"
+send "${passwd}\r"
+set timeout 200
+expect eof
+exit
+EOF
+
+/usr/bin/expect << EOF
+set timeout 200
+spawn ssh ${ip} -o StrictHostKeyChecking=no /root/splunk_install.sh /data wangshan wangshan123
+expect "password"
+send "${passwd}\r"
+set timeout 200
+expect eof
+exit
+EOF
+}
+
+while read ip passwd
+do
+    remotesh
+done < ip.txt
