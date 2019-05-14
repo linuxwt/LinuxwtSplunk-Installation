8326acf591dbabb1d93ece29129a0556a5ad25fa
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-05-14 19:01:38 +0800

    Create splunkforwarder_start.sh

---
+++ splunkforwarder_start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# description: splunk start script
+# chkconfig: 2345 10 90 
+[ -f /etc/init.d/functions ] &&  . /etc/init.d/functions || exit 1  
+[ -f /usr/bin/lsof ] || yum -y install lsof  
+start () {
+    [ $(lsof -i:8089 | wc -l) -gt 0 ] && action "splunk start ..."  /bin/true || /opt/splunkforwarder/bin/splunk start
+    sleep 10
+    [ $(lsof -i:8089 | wc -l) -gt 0 ] && action "splunk start ..."  /bin/true
+    return $RETVAL
+}
+stop () {
+    [ $(lsof -i:8089 | wc -l) -eq 0 ] && action "rsyncd is not running..." /bin/true || /opt/splunkforwarder/bin/splunk stop
+    sleep 10
+    [ $(lsof -i:8089 | wc -l) -eq 0 ] && action "rsyncd is not running..." /bin/true
+    return $RETVAL
+}
+main () {
+    case $1 in
+    start|START)
+          start
+           ;;
+    stop|STOP)
+          stop
+           ;;
+    restart|RESTART)
+          stop
+          sleep 10
+          start
+           ;;
+    *)
+           echo -e "Usage: $0 {start|stop|restart}"
+           exit 1
+           ;;
+    esac
+}
+main $1
+exit $RETVAL
